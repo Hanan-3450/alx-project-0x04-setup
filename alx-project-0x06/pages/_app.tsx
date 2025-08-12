0afd48b88728b880eb166e3647eab95a7e7bef23
Hanan <your-email@example.com>
2025-08-12 06:47:25 -0700

    Implement Redux global state with Header and CounterApp page

--- alx-project-0x06/pages/_app.tsx
+++ alx-project-0x06/pages/_app.tsx
@@ -1,14 +1,14 @@
+// pages/_app.tsx
+import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import store from "@/store/store";
-import Layout from "@/components/layouts/Layout";
-import "@/styles/globals.css";
+import { store } from "../store/store";
+import Header from "../components/layouts/Header";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Header />
+      <Component {...pageProps} />
     </Provider>
   );
 }
--- alx-project-0x06/store/store.ts
+++ alx-project-0x06/store/store.ts
@@ -1,32 +1,12 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
+// store/store.ts
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../slices/counterSlice";
 
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: {
-    value: 0,
-  },
-  reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value > 0 ? (state.value -= 1) : 0;
-    },
-  },
-});
-
-const store = configureStore({
+export const store = configureStore({
   reducer: {
-    counter: counterSlice.reducer,
+    counter: counterReducer,
   },
 });
-
-export const { increment, decrement } = counterSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-
-export default store;
